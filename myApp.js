d6421d17270ef89c13cafb2fca636fc9ad464fbf
millernerd <millernerd@gmail.com>
2021-11-29 14:44:39 -0700

    removed commented code

--- myApp.js
+++ myApp.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-// import * as mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
