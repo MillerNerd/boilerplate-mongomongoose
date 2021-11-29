b7e85ca759fdeebc1ac7c2547ee39e2a7a276b91
millernerd <millernerd@gmail.com>
2021-11-29 15:02:47 -0700

    added personSchema

--- myApp.js
+++ myApp.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: { type: Number, required: true },
+  favoriteFoods: [String]
+})
+
 let Person;
 
 const createAndSavePerson = (done) => {
