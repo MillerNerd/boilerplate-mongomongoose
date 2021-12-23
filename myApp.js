4dfc75135443b6b40c0b28d08808dc242472a3d0
millernerd <millernerd@gmail.com>
2021-12-22 20:07:11 -0700

    fixed by removing unneeded done()

--- myApp.js
+++ myApp.js
@@ -5,20 +5,18 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const personSchema = new mongoose.Schema({
   name: String,
-  age: { type: Number, required: true },
+  age: Number,
   favoriteFoods: [String]
 })
 
 const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  if (err) return console.error(err)
   let bob = new Person({name: "Bob", age: 69, favoriteFoods: ["Pizza", "Ice Cream"]})
   bob.save( (err, data) => {
     if (err) return console.error(err)
     done(null, data)
   })
-  done(null, data);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
