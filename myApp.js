2b3a448a7d1fa3aa061b0a8b81815a133412f961
millernerd <millernerd@gmail.com>
2022-01-11 23:51:13 -0700

    completed

--- myApp.js
+++ myApp.js
@@ -72,7 +72,10 @@ const findAndUpdate = (personName, removePerson) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person(Person.findByIdAndRemove( personId, (err, data) => {
+    if (err) return console.error(err)
+    done(null, data);
+  }))
 };
 
 const removeManyPeople = (done) => {
