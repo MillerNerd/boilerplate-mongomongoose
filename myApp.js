9566f8e5283892035c249b130ce65572e0072653
millernerd <millernerd@gmail.com>
2022-01-12 00:39:05 -0700

    completed

--- myApp.js
+++ myApp.js
@@ -88,8 +88,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person( Person.find( {'favoriteFoods': foodToSearch} )
+                .sort({'name': 1})
+                .limit(2)
+                .select('-age')
+                .exec((err, data) => {
+                  if (err) return console.error(err)
+                  done(null, data)
+                }))
 };
 
 /** **Well Done !!**
